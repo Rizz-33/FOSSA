a59666d82345a370ee7ca1b539732ca110caed97
Risini Amarathunga <162025795+Rizz-33@users.noreply.github.com>
2024-03-13 01:41:37 +0530

    Update main.java

--- main.java
+++ main.java
@@ -1,32 +1,33 @@
-package com.example.project.animals;
-public interface Animal {
-    void makeSound();
-}
-package com.example.project.animals;
+// MyJavaFile.java
 
-public class Dog implements Animal {
-    @Override
-    public void makeSound() {
-        System.out.println("Woof!");
-    }
-}
-package com.example.project.animals;
-public class Cat implements Animal {
-    @Override
-    public void makeSound() {
-        System.out.println("Meow!");
-    }
-}
-package com.example.project;
-import com.example.project.animals.Animal;
-import com.example.project.animals.Dog;
-import com.example.project.animals.Cat;
-public class Main {
+import java.util.Scanner;
+
+public class MyJavaFile {
     public static void main(String[] args) {
-        System.out.println("Welcome to our Java project!");
-        Animal dog = new Dog();
-        Animal cat = new Cat();
-        dog.makeSound();
-        cat.makeSound();
+        System.out.println("Welcome to My Java File!");
+
+        // Ask the user for their name
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+
+        // Greet the user
+        System.out.println("Hello, " + name + "!");
+
+        // Perform a simple calculation
+        int x = 5;
+        int y = 3;
+        int sum = x + y;
+        System.out.println("The sum of " + x + " and " + y + " is: " + sum);
+
+        // Display a message based on the sum
+        if (sum > 10) {
+            System.out.println("That's a big number!");
+        } else {
+            System.out.println("That's not a very big number.");
+        }
+
+        // Farewell message
+        System.out.println("Goodbye, " + name + "!");
     }
 }
