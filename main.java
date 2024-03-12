559c6cc6ebe9e68c3119aeee123ce21006146c90
Risini Amarathunga <162025795+Rizz-33@users.noreply.github.com>
2024-03-13 01:46:10 +0530

    Update main.java

--- main.java
+++ main.java
@@ -12,40 +12,43 @@ public class MyJavaFile {
         // Greet the user
         System.out.println("Hello, " + name + "!");
 
-        // Calculate the Fibonacci sequence
-        System.out.print("Enter the number of Fibonacci numbers to generate: ");
-        int count = scanner.nextInt();
-        System.out.println("The Fibonacci sequence:");
-        for (int i = 0; i < count; i++) {
-            System.out.print(fibonacci(i) + " ");
-        }
-        System.out.println();
+        // Calculate the sum of digits in a number
+        System.out.print("Enter a number to calculate the sum of its digits: ");
+        int number = scanner.nextInt();
+        int sumOfDigits = sumOfDigits(number);
+        System.out.println("The sum of digits in " + number + " is: " + sumOfDigits);
 
-        // Reverse a string
-        scanner.nextLine(); // Consume the newline character
-        System.out.print("Enter a string to reverse: ");
-        String inputString = scanner.nextLine();
-        String reversedString = reverseString(inputString);
-        System.out.println("Reversed string: " + reversedString);
+        // Check if the number is Armstrong
+        if (isArmstrong(number)) {
+            System.out.println(number + " is an Armstrong number.");
+        } else {
+            System.out.println(number + " is not an Armstrong number.");
+        }
 
         // Farewell message
         System.out.println("Goodbye, " + name + "!");
     }
 
-    // Method to calculate the Fibonacci number at a given index
-    private static int fibonacci(int n) {
-        if (n <= 1) {
-            return n;
+    // Method to calculate the sum of digits in a number
+    private static int sumOfDigits(int n) {
+        int sum = 0;
+        while (n != 0) {
+            sum += n % 10;
+            n /= 10;
         }
-        return fibonacci(n - 1) + fibonacci(n - 2);
+        return sum;
     }
 
-    // Method to reverse a string
-    private static String reverseString(String str) {
-        StringBuilder reversed = new StringBuilder();
-        for (int i = str.length() - 1; i >= 0; i--) {
-            reversed.append(str.charAt(i));
+    // Method to check if a number is Armstrong
+    private static boolean isArmstrong(int n) {
+        int originalNumber = n;
+        int digits = String.valueOf(n).length();
+        int sum = 0;
+        while (n != 0) {
+            int digit = n % 10;
+            sum += Math.pow(digit, digits);
+            n /= 10;
         }
-        return reversed.toString();
+        return sum == originalNumber;
     }
 }
