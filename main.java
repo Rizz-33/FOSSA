666857ff22cd7b4023a05a2851264c5ee33ff766
Risini Amarathunga <162025795+Rizz-33@users.noreply.github.com>
2024-03-13 00:15:07 +0530

    Update main.java

--- main.java
+++ main.java
@@ -2,6 +2,7 @@
 package com.example.project;
 
 import com.example.project.util.Utility;
+import com.example.project.data.DataProcessor;
 
 public class Main {
     public static void main(String[] args) {
@@ -14,29 +15,10 @@ public class Main {
         utility.doSomething();
         int result = utility.calculate(5, 3);
         System.out.println("Result of calculation: " + result);
+
+        // Create an instance of DataProcessor class
+        DataProcessor dataProcessor = new DataProcessor();
+        String processedData = dataProcessor.process("input data");
+        System.out.println("Processed data: " + processedData);
     }
 }
-
-// Utility.java (Utility class)
-package com.example.project.util;
-
-public class Utility {
-    public void doSomething() {
-        System.out.println("Utility class is doing something...");
-    }
-    
-    public int calculate(int a, int b) {
-        return a + b;
-    }
-}
-
-// MyClass.java (Another class)
-package com.example.project;
-
-public class MyClass {
-    public void displayMessage() {
-        System.out.println("This is a message from MyClass.");
-    }
-}
-
-
