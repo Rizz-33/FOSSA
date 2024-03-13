db7a3295c2fd608e542fecd6b794a600c0cfa4ac
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-03-13 19:47:59 +0530

    modify: workout data

--- lib/data/workout_data.dart
+++ lib/data/workout_data.dart
@@ -1,7 +1,18 @@
+import 'package:fossa/models/exercise.dart';
 import 'package:fossa/models/workout.dart';
 
 class WorkoutData {
   List<Workout> workoutList = [
-    
+    Workout(
+      name: 'Upper Body',
+      exercises: [
+        Exercise(
+          name: 'Bicep Curls',
+          weight: '10',
+          reps: '10',
+          sets: '3',
+        ),
+      ],
+    )
   ];
 }
