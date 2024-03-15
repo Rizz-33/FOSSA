5785c2e43d8104835809f903d10764f578371432
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-03-15 07:33:22 +0530

    modify: appbar

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,7 +12,17 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('FOSSA'),),
+      appBar: AppBar(
+        title: Text(
+          'FOSSA',
+          style: GoogleFonts.exo2(
+            textStyle: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
