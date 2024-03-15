0a335e9db2bc463f767428b6a0b6e5443ff85720
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-03-15 08:18:43 +0530

    fix: errors

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:fossa/data/workout_data.dart';
+import 'package:fossa/pages/workout_page.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 
